279bc520bfb0e60f8d96db34aa063d85a362f57f
Sebastian <gawron241@gmail.com>
2020-06-05 14:30:42 +0200

    Counting volume of a figure

---
+++ Volume.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Sebastian on 04.06.2020.
+//
+
+#include "Volume.h"
+
+double Volume::countCuboidVolume(double length, double width, double height) {
+    return length * width * height;
+}
---
+++ Volume.h
@@ -0,0 +1,13 @@
+//
+// Created by Sebastian on 04.06.2020.
+//
+
+#ifndef WSO_VOLUME_H
+#define WSO_VOLUME_H
+
+class Volume {
+public:
+    double countCuboidVolume(double length, double width, double height);
+};
+
+#endif //WSO_VOLUME_H
